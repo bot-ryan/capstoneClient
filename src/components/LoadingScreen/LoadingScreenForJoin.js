de53cfe0992d529cc1e53efc1704feda5b516f2f
Ryan <700031613@student.curtin.edu.my>
2022-12-20 22:22:15 +0800

    try to add text color to loading screen

--- src/components/LoadingScreen/LoadingScreenForJoin.js
+++ src/components/LoadingScreen/LoadingScreenForJoin.js
@@ -63,6 +63,7 @@ function LoadingScreenForJoin() {
 
       if(uniquePlayers?.length >= 1) {
         text = players[0]?.name;
+        text.style.color = players[0]?.color;
       }
       for (let i = 1; i < uniquePlayers?.length; i++) {
         text += " | " + uniquePlayers[i]?.name;
